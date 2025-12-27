424592eb569f8c5e4451535407e1a1a4a526c84f
ARandomAxolotl <pmminhtriet2013@gmail.com>
2025-12-27 15:55:19 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,11 +1,13 @@
 @echo off
 if not exist "parental_control_disabler(witten by me).cmd" ( 
 echo The legacy version is NOT detected, launching the AI-debugged version  
-"parental_control_disabler(witten by me but debugged by AI).cmd"
+call "parental_control_disabler(witten by me but debugged by AI).cmd"
+goto :eof
 )
 if not exist "parental_control_disabler(witten by me but debugged by AI).cmd" ( 
 echo The AI-debugged version is NOT detected, launching the LEGACY version  
-"parental_control_disabler(witten by me).cmd"
+call "parental_control_disabler(witten by me).cmd"
+goto :eof
 )
 choice /m "Do you want to launch script witten by me but debugged by AI(it will performs better)?"
 if errorlevel 2 "parental_control_disabler(witten by me).cmd"
